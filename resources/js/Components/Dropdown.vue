f2616b5bc4659bb1592da3961ec88af1610d89a8
Kazim Duran <kazimduran@MacBook-Pro-von-Kazim.local>
2025-12-26 15:18:52 +0100

    Fix: @click.stop entfernt damit Dropdown-Items funktionieren
    - @click.stop entfernt damit Klicks auf Dropdown-Items funktionieren
    - Dropdown schließt sich weiterhin beim Klick außerhalb (durch Overlay)

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -72,12 +72,10 @@ const open = ref(false);
                 v-show="open"
                 class="absolute z-50 mt-2 rounded-md shadow-lg"
                 :class="[widthClass, alignmentClasses]"
-                @click.stop
             >
                 <div
                     class="rounded-md ring-1 ring-black ring-opacity-5"
                     :class="contentClasses"
-                    @click.stop
                 >
                     <slot name="content" />
                 </div>
