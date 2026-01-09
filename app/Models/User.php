f2b86abb7f1fc5c4ad3dce7cdd704c8f76eb72c6
Kazim Duran <kazimduran@MBP-von-Kazim.fritz.box>
2026-01-09 17:37:44 +0100

    Security: Discord Token aus API Responses versteckt
    - discord_token und discord_refresh_token zu $hidden hinzugefügt
    - Verhindert dass sensible Tokens in JSON-Responses zurückgegeben werden

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'discord_token',
+        'discord_refresh_token',
     ];
 
     /**
